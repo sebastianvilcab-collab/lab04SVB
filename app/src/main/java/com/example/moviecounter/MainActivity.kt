61f8009305f4dd141d890b55c07abdcc22b254b6
Sebastian Vilca <sebastian.vilca.b@tecsup.edu.pe>
2026-09-11 17:14:54 -0500

    feat: Add Card component

--- app/src/main/java/com/example/moviecounter/MainActivity.kt
+++ app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -6,17 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.moviecounter.ui.theme.MovieCounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             MovieCounterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -43,5 +46,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     MovieCounterTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun ComponenteCard() {
+    Card(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(
+            text = "Mi primera Card",
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
